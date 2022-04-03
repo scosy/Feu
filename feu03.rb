6114118fc2a51bfe8338c6718f7f361c37089797
scosy <amine.bentaleb064@gmail.com>
2022-04-03 11:15:49 +0200

    WIP

--- feu03.rb
+++ feu03.rb
@@ -2,8 +2,27 @@
 
 # Fonctions utilisées
 def sudoku(unfinished_sudoku)
-  
+  unfinished_sudoku.each.with_index do |row, row_index|
+    row.each.with_index do |number, number_index|
+      if number == "."
+        possible_numbers = find_possible_numbers(unfinished_sudoku, row_index, number_index)
+        
+        exit
+      end
+    end
+  end
+  unfinished_sudoku.map { |v| v.join }
 end
+
+def find_possible_numbers(unfinished_sudoku, row_index, number_index)
+  range = [*1..9]
+  range.map { |number| unfinished_sudoku[row_index].include?(number.to_s) ? () : number }.compact
+end
+
+def check_if_break_line(unfinished_sudoku, row_index, number_index, possible_numbers)
+  possible_numbers.each
+end
+
 # Gestion d'erreurs
 (puts "error"; exit ) if ARGV.size != 1
 (puts "error"; exit ) if !File.file?(ARGV[0])
